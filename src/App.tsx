8557f4c48858f24c8481c97ea74bae63990f31ff
piriabud <eduardo.abudp@uc.cl>
2024-08-01 18:04:37 -0400

    changed switch for route

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Login';
 import Homepage from './Homepage';
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Login} />
-        <Route path="/homepage" component={Homepage} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/homepage" element={<Homepage />} />
+      </Routes>
     </Router>
-  );
-};
+    );
+  };
 
 export default App;
 
